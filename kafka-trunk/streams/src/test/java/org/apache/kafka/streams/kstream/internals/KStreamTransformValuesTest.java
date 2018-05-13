e6c13e354895a75c732049457ca333be51f3ffea
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-05-13 08:41:05 +0200

    first commit

---
+++ kafka-trunk/streams/src/test/java/org/apache/kafka/streams/kstream/internals/KStreamTransformValuesTest.java
@@ -0,0 +1,223 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.streams.kstream.internals;
+
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.Consumed;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.errors.StreamsException;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueTransformer;
+import org.apache.kafka.streams.kstream.ValueTransformerSupplier;
+import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
+import org.apache.kafka.streams.kstream.ValueTransformerWithKeySupplier;
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.processor.To;
+import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.apache.kafka.test.MockProcessorSupplier;
+import org.apache.kafka.test.StreamsTestUtils;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
+
+public class KStreamTransformValuesTest {
+
+    private String topicName = "topic";
+    private final MockProcessorSupplier<Integer, Integer> supplier = new MockProcessorSupplier<>();
+    private final ConsumerRecordFactory<Integer, Integer> recordFactory = new ConsumerRecordFactory<>(new IntegerSerializer(), new IntegerSerializer());
+    private final Properties props = StreamsTestUtils.topologyTestConfig(Serdes.Integer(), Serdes.Integer());
+
+    @Test
+    public void testTransform() {
+        StreamsBuilder builder = new StreamsBuilder();
+
+        ValueTransformerSupplier<Number, Integer> valueTransformerSupplier =
+            new ValueTransformerSupplier<Number, Integer>() {
+                public ValueTransformer<Number, Integer> get() {
+                    return new ValueTransformer<Number, Integer>() {
+
+                        private int total = 0;
+
+                        @Override
+                        public void init(ProcessorContext context) {
+                        }
+
+                        @Override
+                        public Integer transform(Number value) {
+                            total += value.intValue();
+                            return total;
+                        }
+
+                        @Override
+                        public void close() {
+                        }
+                    };
+                }
+            };
+
+        final int[] expectedKeys = {1, 10, 100, 1000};
+
+        KStream<Integer, Integer> stream;
+        stream = builder.stream(topicName, Consumed.with(Serdes.Integer(), Serdes.Integer()));
+        stream.transformValues(valueTransformerSupplier).process(supplier);
+
+        try (final TopologyTestDriver driver = new TopologyTestDriver(builder.build(), props, 0L)) {
+            for (int expectedKey : expectedKeys) {
+                driver.pipeInput(recordFactory.create(topicName, expectedKey, expectedKey * 10, 0L));
+            }
+        }
+        String[] expected = {"1:10", "10:110", "100:1110", "1000:11110"};
+
+        assertArrayEquals(expected, supplier.theCapturedProcessor().processed.toArray());
+    }
+
+    @Test
+    public void testTransformWithKey() {
+        StreamsBuilder builder = new StreamsBuilder();
+
+        ValueTransformerWithKeySupplier<Integer, Number, Integer> valueTransformerSupplier =
+                new ValueTransformerWithKeySupplier<Integer, Number, Integer>() {
+            public ValueTransformerWithKey<Integer, Number, Integer> get() {
+                return new ValueTransformerWithKey<Integer, Number, Integer>() {
+                    private int total = 0;
+                    @Override
+                    public void init(final ProcessorContext context) {
+
+                    }
+                    @Override
+                    public Integer transform(final Integer readOnlyKey, final Number value) {
+                        total += value.intValue() + readOnlyKey;
+                        return total;
+                    }
+
+                    @Override
+                    public void close() {
+
+                    }
+                };
+            }
+        };
+
+        final int[] expectedKeys = {1, 10, 100, 1000};
+
+        KStream<Integer, Integer> stream;
+        stream = builder.stream(topicName, Consumed.with(Serdes.Integer(), Serdes.Integer()));
+        stream.transformValues(valueTransformerSupplier).process(supplier);
+
+        try (final TopologyTestDriver driver = new TopologyTestDriver(builder.build(), props, 0L)) {
+            for (int expectedKey : expectedKeys) {
+                driver.pipeInput(recordFactory.create(topicName, expectedKey, expectedKey * 10, 0L));
+            }
+        }
+        String[] expected = {"1:11", "10:121", "100:1221", "1000:12221"};
+
+        assertArrayEquals(expected, supplier.theCapturedProcessor().processed.toArray());
+    }
+
+
+    @Test
+    public void shouldNotAllowValueTransformerToCallInternalProcessorContextMethods() {
+        final BadValueTransformer badValueTransformer = new BadValueTransformer();
+        final KStreamTransformValues<Integer, Integer, Integer> transformValue = new KStreamTransformValues<>(new ValueTransformerWithKeySupplier<Integer, Integer, Integer>() {
+            @Override
+            public ValueTransformerWithKey<Integer, Integer, Integer> get() {
+                return new ValueTransformerWithKey<Integer, Integer, Integer>() {
+                    @Override
+                    public void init(final ProcessorContext context) {
+                        badValueTransformer.init(context);
+                    }
+
+                    @Override
+                    public Integer transform(final Integer readOnlyKey, final Integer value) {
+                        return badValueTransformer.transform(readOnlyKey, value);
+                    }
+
+                    @Override
+                    public void close() {
+                        badValueTransformer.close();
+                    }
+                };
+            }
+        });
+
+        final Processor transformValueProcessor = transformValue.get();
+        transformValueProcessor.init(null);
+
+        try {
+            transformValueProcessor.process(null, 0);
+            fail("should not allow call to context.forward() within ValueTransformer");
+        } catch (final StreamsException e) {
+            // expected
+        }
+
+        try {
+            transformValueProcessor.process(null, 1);
+            fail("should not allow call to context.forward() within ValueTransformer");
+        } catch (final StreamsException e) {
+            // expected
+        }
+
+        try {
+            transformValueProcessor.process(null, 2);
+            fail("should not allow call to context.forward() within ValueTransformer");
+        } catch (final StreamsException e) {
+            // expected
+        }
+
+        try {
+            transformValueProcessor.process(null, 3);
+            fail("should not allow call to context.forward() within ValueTransformer");
+        } catch (final StreamsException e) {
+            // expected
+        }
+    }
+
+    private static final class BadValueTransformer implements ValueTransformerWithKey<Integer, Integer, Integer> {
+        private ProcessorContext context;
+
+        @Override
+        public void init(final ProcessorContext context) {
+            this.context = context;
+        }
+
+        @Override
+        public Integer transform(final Integer key, final Integer value) {
+            if (value == 0) {
+                context.forward(null, null);
+            }
+            if (value == 1) {
+                context.forward(null, null, (String) null);
+            }
+            if (value == 2) {
+                context.forward(null, null, 0);
+            }
+            if (value == 3) {
+                context.forward(null, null, To.all());
+            }
+            throw new RuntimeException("Should never happen in this test");
+        }
+
+        @Override
+        public void close() { }
+    }
+}
---
+++ kafka.md
@@ -0,0 +1,75 @@
+# Apache Kafka
+
+Apache Kafka helps you decouple your data streams. It is a distributed system. 
+
+How we use it at the time of writing...
+
+## General concepts
+
+### Topics
+
+- Topics are a stream of data, kind of like a database without all the constraints
+- One can have as many topics as you want
+- topic is identified by it name `logfire-cpt-iht`
+- Topics are split into partitions
+- each partitions is ordered
+- each message within a partition gets and incremental id, called and offset
+- once data has been written to a partition it cannot be changed aka `IMMUTABILITY`
+
+## Brokers
+
+- A Kafka cluster is composed of multiple brokers (servers)
+- broker is identified by an ID
+- after connecting to any broker (called bootstrap broker), you are effectively connected to the entire cluster
+- if one broker dies, we will still have 2 live brokers to serve data
+
+## Leader
+
+At any given time only one can only have a leader. Only that leader can receive and send data, all other brokers copy whats going on in the leader.
+
+## Producer
+
+Producers write data to a topic. They only have to specif a topic name and the data will be written to the broker.
+Producers can choos to recievec 
+
+### ssh into kafka pod
+
+docker run --rm -it --net=host landoop/fast-data-dev bash
+
+## What is it
+
+Kafka is a message a streaming data pipeline.
+
+### Commands
+
+Here are some basic commands once you have got an instance of Kafka running.
+
+#### Create topic
+
+```bash
+kafka-topics --zookeeper 127.0.0.1:2181 --create --topic first_topic --partitions 3 --replication-factor 1
+```
+
+#### List
+
+```bash
+kafka-topics --zookeeper 127.0.0.1:2181 --list
+```
+
+#### Delete topic
+
+```bash
+kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --delete
+```
+
+#### Describe topic
+
+```bash
+kafka-topics --zookeeper 127.0.0.1:2181 --topic first_topic --describe
+```
+
+#### Consuming data from from topic using console
+
+```bash
+kafka-console-consumer --botstrap-server 127.0.0.1:9092 --topic first_topic --from-beginning
+```
